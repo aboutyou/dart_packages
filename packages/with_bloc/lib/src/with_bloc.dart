8e6319a10f13c70742a596e536e6e612e48736ae
Henri Beck <henribeck.dev@gmail.com>
2019-10-22 22:16:57 +0200

    Create inline docs for WithBloc

--- packages/with_bloc/lib/src/with_bloc.dart
+++ packages/with_bloc/lib/src/with_bloc.dart
@@ -4,20 +4,35 @@ import 'package:flutter/material.dart';
 
 const listId = ListEquality<dynamic>();
 
+/// A helper widget for easily creating [StateQueue] in the [build] method.
+///
+/// This widget takes care of creating the [BlocType] and disposing it when necessary.
+/// It also handles recreating the [BlocType] whenever any [inputs] change.
 class WithBloc<BlocType extends ValueNotifier<StateType>, StateType>
     extends StatefulWidget {
   const WithBloc({
     Key key,
     @required this.createBloc,
-    this.inputs,
     @required this.builder,
+    this.inputs = const [],
     this.child,
   })  : assert(createBloc != null),
         assert(builder != null),
+        assert(inputs != null),
         super(key: key);
 
+  /// A function which creates the [BlocType] instance
+  ///
+  /// This function might be called multiple times in the lifetime of this widget.
+  /// Whenever the [inputs] change, a new bloc will be created by calling this function.
   final BlocType Function(BuildContext context) createBloc;
 
+  /// A [Function] which builds a widget depending on the [BlocType] and [StateType].
+  ///
+  /// The [bloc] will be the current bloc and [value] is the current [StateType].
+  /// When the bloc emits a new value, this function will be called again to reflect the changes in the UI.
+  ///
+  /// The [child] will be passed in as the fourth argument and might be `null` if not supplied.
   final Widget Function(
     BuildContext context,
     BlocType bloc,
@@ -25,6 +40,15 @@ class WithBloc<BlocType extends ValueNotifier<StateType>, StateType>
     Widget child,
   ) builder;
 
+  /// A [BlocType]-independent widget which is passed back to the [builder].
+  ///
+  /// This argument is optional and can be null if the entire widget subtree
+  /// the [builder] builds depends on the value of the [BlocType].
+  ///
+  /// Can be `null`.
+  ///
+  /// See also:
+  ///  - [ValueListenableBuilder]
   final Widget child;
 
   /// The parameters the BLoC depends upon.
