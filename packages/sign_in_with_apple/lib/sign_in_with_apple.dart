1b81d9728baf326802be1eb3e0ccd8eddd1835c5
Timm Preetz <timm@preetz.us>
2020-03-11 16:51:11 +0100

    Split up files, add basic channel test

--- packages/sign_in_with_apple/lib/sign_in_with_apple.dart
+++ packages/sign_in_with_apple/lib/sign_in_with_apple.dart
@@ -1,9 +1,18 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:meta/meta.dart';
 
-export './sign_in_with_apple_button/sign_in_with_apple_button.dart';
+import './authorization_credential.dart';
+import './credential_state.dart';
+
+export './authorization_credential.dart'
+    show
+        AuthorizationCredential,
+        AuthorizationCredentialAppleID,
+        AuthorizationCredentialPassword;
+export './credential_state.dart' show CredentialState;
+export './sign_in_with_apple_button/sign_in_with_apple_button.dart'
+    show SignInWithAppleButton, SignInWithAppleButtonStyle, IconAlignment;
 
 class SignInWithApple {
   static const MethodChannel _channel =
@@ -17,7 +26,7 @@ class SignInWithApple {
   ///
   /// Successful result will be either of type [AuthorizationCredentialAppleID] or [AuthorizationCredentialPassword]
   static Future<AuthorizationCredential> requestCredentials() async {
-    return _parseCredentialsResponse(
+    return parseCredentialsResponse(
       await _channel
           .invokeMethod<Map<dynamic, dynamic>>('performAuthorizationRequest'),
     );
@@ -26,118 +35,11 @@ class SignInWithApple {
   static Future<CredentialState> getCredentialState(
     String userIdentifier,
   ) async {
-    return _parseCredentialState(
+    return parseCredentialState(
       await _channel.invokeMethod<String>(
         'getCredentialState',
         <String, String>{'userIdentifier': userIdentifier},
       ),
     );
-  }
-}
-
-enum CredentialState {
-  authorized,
-  revoked,
-  notFound,
-}
-
-CredentialState _parseCredentialState(String credentialState) {
-  switch (credentialState) {
-    case 'authorized':
-      return CredentialState.authorized;
-
-    case 'revoked':
-      return CredentialState.revoked;
-
-    case 'notFound':
-      return CredentialState.notFound;
-
-    default:
-      throw Exception('Unsupported credential state $credentialState');
-  }
-}
-
-@immutable
-abstract class AuthorizationCredential {}
-
-class AuthorizationCredentialAppleID implements AuthorizationCredential {
-  AuthorizationCredentialAppleID({
-    @required this.userIdentifier,
-    @required this.givenName,
-    @required this.familyName,
-    @required this.email,
-    @required this.identityToken,
-    @required this.authorizationCode,
-  })  : assert(userIdentifier != null),
-        assert(identityToken != null),
-        assert(authorizationCode != null);
-
-  final String userIdentifier;
-
-  /// Can be `null`, will only be returned on the first authorization
-  final String givenName;
-
-  /// Can be `null`, will only be returned on the first authorization
-  final String familyName;
-
-  /// Can be `null`, will only be returned on the first authorization
-  final String email;
-
-  /// Can be `null` on the native side, but is expected on the Flutter side,
-  /// as the authorization would be useless without the tokens to validate them
-  /// on your own server
-  final String identityToken;
-
-  /// Can be `null` on the native side, but is expected on the Flutter side,
-  /// as the authorization would be useless without the tokens to validate them
-  /// on your own server
-  final String authorizationCode;
-
-  @override
-  String toString() {
-    return 'AuthorizationAppleID($userIdentifier, $givenName, $familyName, $email, identityToken set? ${identityToken != null}, authorizationCode set? ${authorizationCode != null})';
-  }
-}
-
-class AuthorizationCredentialPassword implements AuthorizationCredential {
-  AuthorizationCredentialPassword({
-    @required this.username,
-    @required this.password,
-  })  : assert(username != null),
-        assert(password != null);
-
-  final String username;
-
-  final String password;
-
-  @override
-  String toString() {
-    return 'AuthorizationCredential($username, [REDACTED PASSWORD])';
-  }
-}
-
-// ignore_for_file: avoid_as
-AuthorizationCredential _parseCredentialsResponse(
-  Map<dynamic, dynamic> response,
-) {
-  switch (response['type'] as String) {
-    case 'appleid':
-      return AuthorizationCredentialAppleID(
-        userIdentifier: response['userIdentifier'] as String,
-        givenName: response['givenName'] as String,
-        familyName: response['familyName'] as String,
-        email: response['email'] as String,
-        identityToken: response['identityToken'] as String,
-        authorizationCode: response['authorizationCode'] as String,
-      );
-
-    case 'password':
-      return AuthorizationCredentialPassword(
-        username: response['username'] as String,
-        password: response['password'] as String,
-      );
-
-    default:
-      throw Exception('Unsupported result type ${response['type']}');
   }
 }
--- packages/sign_in_with_apple/test/sign_in_with_apple_test.dart
+++ packages/sign_in_with_apple/test/sign_in_with_apple_test.dart
@@ -1,23 +1,55 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-// import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 void main() {
-  const channel = MethodChannel('sign_in_with_apple');
+  const channel = MethodChannel('de.aboutyou.mobile.app.sign_in_with_apple');
 
   TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((methodCall) async {
-      return '42';
-    });
-  });
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    // expect(await SignInWithApple.platformVersion, '42');
+  test('performAuthorizationRequest -> Apple ID', () async {
+    channel.setMockMethodCallHandler((methodCall) async {
+      if (methodCall.method == 'performAuthorizationRequest') {
+        return <dynamic, dynamic>{
+          'type': 'appleid',
+          'userIdentifier': 'some userIdentifier',
+          'givenName': 'some givenName',
+          'familyName': 'some familyName',
+          'email': 'some@email.com',
+          'identityToken': 'identityToken',
+          'authorizationCode': 'authorizationCode',
+        };
+      }
+
+      throw Exception('Unexpected method');
+    });
+
+    expect(
+      await SignInWithApple.requestCredentials(),
+      isA<AuthorizationCredentialAppleID>(),
+    );
+  });
+
+  test('performAuthorizationRequest -> Username/Password', () async {
+    channel.setMockMethodCallHandler((methodCall) async {
+      if (methodCall.method == 'performAuthorizationRequest') {
+        return <dynamic, dynamic>{
+          'type': 'password',
+          'username': 'user1',
+          'password': 'admin',
+        };
+      }
+
+      throw Exception('Unexpected method');
+    });
+
+    expect(
+      await SignInWithApple.requestCredentials(),
+      isA<AuthorizationCredentialPassword>(),
+    );
   });
 }
