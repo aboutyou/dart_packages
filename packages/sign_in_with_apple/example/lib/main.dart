5061056a45538b2898b47ec93e446dff64448b0e
Timm Preetz <timm@preetz.us>
2020-03-29 13:41:30 +0200

    Remove workaround which is no longer needed

--- packages/sign_in_with_apple/example/lib/main.dart
+++ packages/sign_in_with_apple/example/lib/main.dart
@@ -2,16 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
-import 'dart:io' show Platform;
-import 'package:flutter/foundation.dart'
-    show debugDefaultTargetPlatformOverride;
-
 void main() {
-  // Workaround for macOS support (https://github.com/flutter/flutter/issues/39881)
-  if (!Platform.isAndroid && !Platform.isIOS) {
-    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
-  }
-
   runApp(MyApp());
 }
 
@@ -31,22 +22,22 @@ class _MyAppState extends State<MyApp> {
         body: Container(
           padding: EdgeInsets.all(10),
           child: Center(
-          child: SignInWithAppleButton(
-            onPressed: () async {
-              final credentials = await SignInWithApple.requestCredentials();
+            child: SignInWithAppleButton(
+              onPressed: () async {
+                final credentials = await SignInWithApple.requestCredentials();
 
-              print(credentials);
+                print(credentials);
 
-              if (credentials is AuthorizationCredentialAppleID) {
-                /// send credentials to your server to create a session
-                /// after they have been validated with Apple
-              } else if (credentials is AuthorizationCredentialPassword) {
-                /// Login the user using username/password combination
-              }
-            },
+                if (credentials is AuthorizationCredentialAppleID) {
+                  /// send credentials to your server to create a session
+                  /// after they have been validated with Apple
+                } else if (credentials is AuthorizationCredentialPassword) {
+                  /// Login the user using username/password combination
+                }
+              },
+            ),
           ),
         ),
-      ),
       ),
     );
   }
