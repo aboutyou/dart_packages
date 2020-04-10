92f98ca6106f4ebb9938e911f851da113ce8388e
HenriBeck <henri.beck@aboutyou.com>
2020-04-10 20:29:38 +0200

    Add more docs to the AuthorizationRequest

--- packages/sign_in_with_apple/lib/src/authorization_request.dart
+++ packages/sign_in_with_apple/lib/src/authorization_request.dart
@@ -1,5 +1,12 @@
 import 'package:meta/meta.dart';
 
+/// A base class which describes an authorization request that we will make on the native side.
+///
+/// Apple Docs: https://developer.apple.com/documentation/authenticationservices/asauthorizationrequest
+///
+/// We currently only support the following authorization requests:
+/// - [AppleIDAuthorizationRequest]
+/// - [PasswordAuthorizationRequest]
 @immutable
 abstract class AuthorizationRequest {
   const AuthorizationRequest();
@@ -7,11 +14,20 @@ abstract class AuthorizationRequest {
   Map<String, dynamic> toJson();
 }
 
+/// The scopes that will be requested with the [AppleIDAuthorizationRequest].
+/// This allows you to request additional information from the user upon sign up.
+///
+/// Apple Docs: https://developer.apple.com/documentation/authenticationservices/asauthorization/scope
 enum AppleIDAuthorizationScopes {
   email,
   fullName,
 }
 
+/// An [AuthorizationRequest] which authenticates a user based on their Apple ID.
+///
+/// Apple Docs:
+/// - https://developer.apple.com/documentation/authenticationservices/asauthorizationappleidprovider
+/// - https://developer.apple.com/documentation/authenticationservices/asauthorizationappleidrequest
 class AppleIDAuthorizationRequest implements AuthorizationRequest {
   const AppleIDAuthorizationRequest({
     this.scopes = const [],
@@ -44,6 +60,11 @@ class AppleIDAuthorizationRequest implements AuthorizationRequest {
   }
 }
 
+/// An [AuthorizationRequest] that uses credentials which are stored in the users Keychain.
+///
+/// Apple Docs:
+/// - https://developer.apple.com/documentation/authenticationservices/asauthorizationpasswordprovider
+/// - https://developer.apple.com/documentation/authenticationservices/asauthorizationpasswordrequest
 class PasswordAuthorizationRequest implements AuthorizationRequest {
   const PasswordAuthorizationRequest();
 
