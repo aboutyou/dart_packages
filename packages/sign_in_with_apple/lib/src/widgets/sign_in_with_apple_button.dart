70411146fc25cff2510072cb1ef614d11c6386b6
HenriBeck <henri.beck@aboutyou.com>
2020-04-21 22:13:01 +0200

    Add more docs and safety to the sign in with apple button

--- packages/sign_in_with_apple/lib/src/widgets/sign_in_with_apple_button.dart
+++ packages/sign_in_with_apple/lib/src/widgets/sign_in_with_apple_button.dart
@@ -2,7 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sign_in_with_apple/src/widgets/apple_logo_painter.dart';
 
-/// Style according to
+const _appleIconSize = 28.0;
+
+/// A `Sign in with Apple` button according to the Apple Guidelines.
+///
 /// https://developer.apple.com/design/human-interface-guidelines/sign-in-with-apple/overview/buttons/
 class SignInWithAppleButton extends StatelessWidget {
   const SignInWithAppleButton({
@@ -20,16 +23,26 @@ class SignInWithAppleButton extends StatelessWidget {
         assert(iconAlignment != null),
         super(key: key);
 
+  /// The callback which will be called when the button is pressed.
   final VoidCallback onPressed;
 
+  /// The text to the
   final String text;
 
+  /// The height of the button.
+  ///
+  /// This default to `44` according to the Apple Guidelines.
   final double height;
 
+  /// The style of the button
   final SignInWithAppleButtonStyle style;
 
+  /// The border radius of the button.
+  ///
+  /// Defaults to `8` pixels.
   final BorderRadius borderRadius;
 
+  /// How the icon should be aligned inside the button
   final IconAlignment iconAlignment;
 
   @override
@@ -38,6 +51,36 @@ class SignInWithAppleButton extends StatelessWidget {
         style == SignInWithAppleButtonStyle.black ? Colors.black : Colors.white;
     final contrastColor =
         style == SignInWithAppleButtonStyle.black ? Colors.white : Colors.black;
+
+    // per Apple's guidelines
+    final fontSize = height * 0.43;
+
+    final textWidget = Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+        color: contrastColor,
+        // defaults styles aligned with https://github.com/flutter/flutter/blob/master/packages/flutter/lib/src/cupertino/text_theme.dart#L16
+        fontFamily: '.SF Pro Text',
+        letterSpacing: -0.41,
+      ),
+    );
+
+    final appleIcon = Container(
+      width: _appleIconSize,
+      height: _appleIconSize,
+      child: Center(
+        child: Container(
+          width: fontSize * (25 / 31),
+          height: fontSize,
+          child: CustomPaint(
+            painter: AppleLogoPainter(
+              color: contrastColor,
+            ),
+          ),
+        ),
+      ),
+    );
 
     return Container(
       height: height,
@@ -59,40 +102,22 @@ class SignInWithAppleButton extends StatelessWidget {
             height: height,
             child: Row(
               children: <Widget>[
-                Container(
-                  width: 28,
-                  height: 28,
-                  child: Center(
-                    child: Container(
-                      width: height * 0.43 * (25 / 31),
-                      height: height * 0.43,
-                      child: CustomPaint(
-                        painter: AppleLogoPainter(
-                          color: contrastColor,
-                        ),
-                      ),
-                    ),
+                if (iconAlignment == IconAlignment.center) ...[
+                  appleIcon,
+                  Flexible(
+                    child: textWidget,
                   ),
-                ),
-                Text(
-                  text,
-                  style: TextStyle(
-                    fontSize: height * 0.43 /* per Apple's guidelines */,
-                    color: contrastColor,
-                    // defaults styles aligned with https://github.com/flutter/flutter/blob/master/packages/flutter/lib/src/cupertino/text_theme.dart
-                    fontFamily: '.SF Pro Text',
-                    letterSpacing: -0.41,
+                ] else if (iconAlignment == IconAlignment.left) ...[
+                  appleIcon,
+                  Expanded(
+                    child: textWidget,
                   ),
-                ),
-                if (iconAlignment == IconAlignment.left)
-                  // so text gets center aligned
-                  Container(
-                    width: 28,
-                  )
+                  SizedBox(
+                    width: _appleIconSize,
+                  ),
+                ],
               ],
-              mainAxisAlignment: iconAlignment == IconAlignment.left
-                  ? MainAxisAlignment.spaceBetween
-                  : MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
             ),
           ),
           onPressed: onPressed,
