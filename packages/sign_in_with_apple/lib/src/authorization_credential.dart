6470e1debf60ea9202da41619620ab9e27e1bc1b
HenriBeck <henri.beck@aboutyou.com>
2020-04-27 21:54:34 +0200

    Add identity token

--- packages/sign_in_with_apple/lib/src/authorization_credential.dart
+++ packages/sign_in_with_apple/lib/src/authorization_credential.dart
@@ -156,8 +156,6 @@ AuthorizationCredentialAppleID parseAuthorizationCredentialAppleIDFromDeeplink(
     givenName: user != null ? user['name']['firstName'] as String : null,
     familyName: user != null ? user['name']['lastName'] as String : null,
     userIdentifier: null,
-
-    /// TODO: Do we get this here
-    identityToken: null,
+    identityToken: deeplink.queryParameters['id_token'],
   );
 }
