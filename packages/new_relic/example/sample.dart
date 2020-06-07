fdeafc3b4fe64d1ff27d91dad188be5e98d57364
Timm Preetz <timm@preetz.us>
2020-06-07 16:43:04 +0200

    Add some variance to sample data

--- packages/new_relic/example/sample.dart
+++ packages/new_relic/example/sample.dart
@@ -1,7 +1,11 @@
+import 'dart:math';
+
 import '../lib/src/app.dart' show NRApp;
 import 'dart:io' show Platform;
 
 void main() async {
+  final random = Random();
+
   final app = NRApp(
     cSDKPath: "/app/libnewrelic.so",
     daemonHost: "newrelic:7878",
@@ -9,18 +13,20 @@ void main() async {
     licenseKey: Platform.environment["NEW_RELIC_LICENSE_KEY"],
   );
 
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 500; i++) {
     try {
       final txn = app.startWebTransaction(
         ("GET /Sample/Dart/Transaction"),
       );
 
-      await Future<void>.delayed(Duration(seconds: 1));
+      await Future<void>.delayed(
+        Duration(milliseconds: 500 + random.nextInt(2500)),
+      );
 
-      txn.addAttribute("int_value", 123457);
+      txn.addAttribute("int_value", random.nextInt(100000));
       txn.addAttribute("string_value", "foobar");
 
-      if (i % 3 == 0) {
+      if (random.nextInt(10) == 0) {
         txn.noticeError(
           Exception("Test"),
           StackTrace.current,
