eda72c05016e06005e13fa23068483e656a5b9d1
HenriBeck <henri.beck@aboutyou.com>
2024-04-06 17:34:15 +0200

    chore: more iOS changes

--- packages/sign_in_with_apple/sign_in_with_apple/example/lib/generated_plugin_registrant.dart
+++ packages/sign_in_with_apple/sign_in_with_apple/example/lib/generated_plugin_registrant.dart
@@ -5,7 +5,7 @@
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:sign_in_with_apple_web/sign_in_with_apple_web.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
