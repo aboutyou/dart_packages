da65b5c45eb0f854acc6589ee098d5197f6ec394
oussama berhili <oussamaberhili190@gmail.com>
2025-01-22 15:13:06 +0100

    Update dependencies and project configuration for sign_in_with_apple example
    - Upgraded various package versions in pubspec.lock and pubspec.yaml, including:
      - collection: 1.18.0 to 1.19.0
      - leak_tracker: 10.0.0 to 10.0.7
      - leak_tracker_flutter_testing: 2.0.1 to 3.0.8
      - flutter_lints: 3.0.1 to 5.0.0
      - http: 1.2.1 to 1.3.0
      - vm_service: 13.0.0 to 14.3.0
      - dart SDK: updated to >=3.4.0 <4.0.0
    - Updated Android and iOS project configurations, including Gradle and CMake files.
    - Refactored AndroidManifest.xml and other Android files for better compatibility.
    - Added new files for Linux and Windows support, including CMake configurations and resource files.
    - Removed outdated Kotlin MainActivity and replaced it with a Java implementation.
    - Added basic test files for iOS and Windows platforms.
    
    This commit enhances compatibility and prepares the project for future updates.

---
+++ packages/sign_in_with_apple/sign_in_with_apple/example/android/app/src/main/java/com/example/sign_in_with_apple_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.sign_in_with_apple_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- packages/sign_in_with_apple/sign_in_with_apple/example/android/app/src/main/kotlin/com/aboutyou/dart_packages/sign_in_with_apple/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.aboutyou.dart_packages.sign_in_with_apple.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
