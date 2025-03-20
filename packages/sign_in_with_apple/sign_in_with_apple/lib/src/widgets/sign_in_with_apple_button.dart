c411cb9a9b3c1943aa4e290d83f9555793634afb
Indigosoftwares21 <indigosoftwares21@gmail.com>
2025-03-20 15:37:03 +0200

    Revert "Update dependencies and add textWidget parameter to SignInWithAppleButton"
    This reverts commit c9bab8000f42c830f750aec22659fe94d7ccc620.

--- packages/sign_in_with_apple/sign_in_with_apple/lib/src/widgets/sign_in_with_apple_button.dart
+++ packages/sign_in_with_apple/sign_in_with_apple/lib/src/widgets/sign_in_with_apple_button.dart
@@ -13,7 +13,6 @@ class SignInWithAppleButton extends StatelessWidget {
     super.key,
     required this.onPressed,
     this.text = 'Sign in with Apple',
-    this.textWidget,
     this.height = 44,
     this.style = SignInWithAppleButtonStyle.black,
     this.borderRadius = const BorderRadius.all(Radius.circular(8.0)),
@@ -49,11 +48,6 @@ class SignInWithAppleButton extends StatelessWidget {
   ///
   /// This defaults to [IconAlignment.center].
   final IconAlignment iconAlignment;
-
-  /// The text widget to display next to the Apple logo.
-  ///
-  /// If this is set, the [text] parameter will be ignored.
-  final Widget? textWidget;
 
   /// Returns the background color of the button based on the current [style].
   Color get _backgroundColor {
@@ -101,20 +95,18 @@ class SignInWithAppleButton extends StatelessWidget {
     // per Apple's guidelines
     final fontSize = height * 0.43;
 
-    // The text widget to display next to the Apple logo
-    final textWidget = this.textWidget ??
-        Text(
-          text,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            inherit: false,
-            fontSize: fontSize,
-            color: _contrastColor,
-            // defaults styles aligned with https://github.com/flutter/flutter/blob/master/packages/flutter/lib/src/cupertino/text_theme.dart#L16
-            fontFamily: '.SF Pro Text',
-            letterSpacing: -0.41,
-          ),
-        );
+    final textWidget = Text(
+      text,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        inherit: false,
+        fontSize: fontSize,
+        color: _contrastColor,
+        // defaults styles aligned with https://github.com/flutter/flutter/blob/master/packages/flutter/lib/src/cupertino/text_theme.dart#L16
+        fontFamily: '.SF Pro Text',
+        letterSpacing: -0.41,
+      ),
+    );
 
     final appleIcon = Container(
       width: _appleIconSizeScale * height,
